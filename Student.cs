2945838a7cb3e47b71421ca01f771f15f3939327
GiaHan71 <hn230736@gmail.com>
2025-11-21 11:41:58 +0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Studentcpp
+{
+    internal class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Student(int id, string name, int age)
+            {
+                Id = id;
+                Name = name;
+                Age = age;
+            }
+        }
+    }
+
